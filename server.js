0e297478e8e0d05ab2eaa25f58988afde2a81f5a
Zach Drummond <zachdrummond3@gmail.com>
2020-10-08 09:50:26 -0400

    Added handlebars folders

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// MIDDLEWARE
+app.use(express.urlencoded( { extended: true } ) );
+app.use(express.json());
+TODO: // Add the Path to the public file
+// app.use(express.static(path.join(__dirname, (path))));
+app.engine("handlebars", exphbs( { defaultLayout: "main" } ) );
+app.set("view engine", "handlebars");
+
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 })
 
-app.get("/api/config", (req, res) => {
-    res.json({
-        success: true
-    });
+app.get("/", (req, res) => {
+    res.render("index");
 });
