b854f33177393beba370b67c060a678b0dd774dc
Zach Drummond <zachdrummond3@gmail.com>
2020-10-08 19:01:23 -0400

    Updated server.js by adding app.use(express.static(public))

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
+// Dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
 const connection = require("./config/connection");
+const routes = require("./controllers/burgers_controllers.js");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// MIDDLEWARE
-app.use(express.urlencoded( { extended: true } ) );
-app.use(express.json());
-TODO: // Add the Path to the public file
-// app.use(express.static(path.join(__dirname, (path))));
-app.engine("handlebars", exphbs( { defaultLayout: "main" } ) );
-app.set("view engine", "handlebars");
-
+// Starts the server and tells it to listen to client requests
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 })
 
-app.get("/", (req, res) => {
-    connection.query("SELECT * FROM burgers", (err, data) => {
-        console.log(data);
-        res.render("index", { data: data});
-    });
-});
+// MIDDLEWARE
+app.use(express.urlencoded( { extended: true } ) );
+app.use(express.json());
+app.use(express.static("public"));
+app.engine("handlebars", exphbs( { defaultLayout: "main" } ) );
+app.set("view engine", "handlebars");
+app.use(routes);
