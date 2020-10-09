ad0def79189639476464420831cbe86131eb17c6
Zach Drummond <zachdrummond3@gmail.com>
2020-10-09 17:54:20 -0400

    Added comments to controller

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,24 +1,27 @@
+// Dependencies
 const express = require("express");
 const burger = require("../models/burger.js");
 
 const router = express.Router();
 
+// Read Route
 router.get("/", function (request, response) {
   burger.all(function (data) {
     response.render("index", { burgers: data });
   });
 });
 
+// Create Route
 router.post("/api/burgers", function (request, response) {
   burger.create(request.body.burger_name, function (result) {
     response.json({ id: result.insertId }); // Send back the id of the new burger
   });
 });
 
+// Update Route
 router.put("/api/burgers/:id", function (request, response) {
   burger.update(request.params.id, function (result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return response.status(404).end();
     } else {
       response.status(200).end();
@@ -26,10 +29,10 @@ router.put("/api/burgers/:id", function (request, response) {
   });
 });
 
+// Delete Route
 router.delete("/api/burgers/:id", function (request, response) {
     burger.delete(request.params.id, function (result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return response.status(404).end();
     } else {
       response.status(200).end();
