5de2b1cd707883d19b12f00cb0d596c29c7959d5
Zach Drummond <zachdrummond3@gmail.com>
2020-10-09 18:01:16 -0400

    added comments to burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,21 +2,25 @@ const orm = require("../config/orm");
 
 // Creates a Burger Model
 const burger = {
+    // A method that calls the orm's selectAll method
     all: function(callback) {
         orm.selectALL("burgers", function(result){
             callback(result);
         });
     },
+    // A method that calls the orm's insertOne method
     create: function(value, callback){
         orm.insertOne("burgers", "burger_name", value, function(result){
             callback(result);
         });
     },
+    // A method that calls the orm's updateOne method
     update: function(id, callback){
         orm.updateOne("burgers", "devoured", 1, id, function(result){
             callback(result);
         });
     },
+    // A method that calls the orm's deleteOne method
     delete: function(id, callback){
         orm.deleteOne("burgers", id, function(result){
             callback(result);
